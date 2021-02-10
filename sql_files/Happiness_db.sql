33ded74660007fd3bf1bbfa107731f6ae66d97a8
Kpearson72 <kpearson72@gmail.com>
2021-02-10 03:02:07 -0600

    commit combining_data_columns notebook and deleting untitled notebook

--- sql_files/Happiness_db.sql
+++ sql_files/Happiness_db.sql
@@ -5,11 +5,11 @@ DROP TABLE IF EXISTS gov_response;
 DROP TABLE IF EXISTS dept_manager;
 DROP TABLE IF EXISTS world_covid_data;
 DROP TABLE IF EXISTS world_happiness;
-DROP TABLE IF EXISTS un_government;
+DROP TABLE IF EXISTS systems_govt ;
 
 
 CREATE TABLE systems_govt (
-	id SERIAL PRIMARY KEY,
+	id INT PRIMARY KEY,
     name VARCHAR NOT NULL,
     head_of_state VARCHAR NOT NULL,
     gov_type VARCHAR NOT NULL
@@ -17,7 +17,7 @@ CREATE TABLE systems_govt (
 
 CREATE TABLE world_happiness (
     country_id INT PRIMARY KEY,
-	FOREIGN KEY (country_id) REFERENCES un_government(id),
+	FOREIGN KEY (country_id) REFERENCES systems_govt (id),
     happiness_rank FLOAT   NOT NULL,
     social_support FLOAT   NOT NULL,
     healthy_life_expectancy FLOAT   NOT NULL,
@@ -28,18 +28,18 @@ CREATE TABLE world_happiness (
 );
 
 CREATE TABLE world_covid_data (
-	covid_cases_id SERIAL PRIMARY KEY,
+	covid_cases_id INT PRIMARY KEY,
     country_id INT NOT NULL,
-	FOREIGN KEY (country_id)REFERENCES un_government(id),
+	FOREIGN KEY (country_id)REFERENCES systems_govt (id),
     date DATE   NOT NULL,
     new_cases INT   NOT NULL,
     new_deaths INT   NOT NULL
 );
 
 CREATE TABLE gov_response (
-	response_id SERIAL PRIMARY KEY,
+	response_id INT PRIMARY KEY,
     country_id INT NOT NULL,
-	FOREIGN KEY (country_id)REFERENCES un_government(id),
+	FOREIGN KEY (country_id)REFERENCES systems_govt (id),
     gov_resp_date DATE   NOT NULL,
     gov_resp_desc VARCHAR   NOT NULL,
     gov_resp_type VARCHAR   NOT NULL,
