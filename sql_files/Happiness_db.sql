b941f5da6cd655906e9306222a7b0752f30ff417
Kpearson72 <kpearson72@gmail.com>
2021-02-10 06:51:40 -0600

    add ERD, finished Happiness_db and copy of working notebook with fixed code

--- sql_files/Happiness_db.sql
+++ sql_files/Happiness_db.sql
@@ -2,23 +2,24 @@
 
 
 DROP TABLE IF EXISTS gov_response;
-DROP TABLE IF EXISTS dept_manager;
 DROP TABLE IF EXISTS world_covid_data;
 DROP TABLE IF EXISTS world_happiness;
-DROP TABLE IF EXISTS systems_govt ;
+DROP TABLE IF EXISTS un_govt ;
 
 
-CREATE TABLE systems_govt (
+CREATE TABLE un_govt (
 	id INT PRIMARY KEY,
-    name VARCHAR NOT NULL,
+    country VARCHAR NOT NULL,
     head_of_state VARCHAR NOT NULL,
-    gov_type VARCHAR NOT NULL
+	constitutional_form VARCHAR NOT NULL,
+    basis_of_executive_legitimacy VARCHAR NOT NULL
 );
 
 CREATE TABLE world_happiness (
     country_id INT PRIMARY KEY,
-	FOREIGN KEY (country_id) REFERENCES systems_govt (id),
-    happiness_rank FLOAT   NOT NULL,
+	FOREIGN KEY (country_id) REFERENCES un_govt(id),
+    world_region VARCHAR NOT NULL,
+	happiness_score FLOAT   NOT NULL,
     social_support FLOAT   NOT NULL,
     healthy_life_expectancy FLOAT   NOT NULL,
     freedom_of_choice FLOAT   NOT NULL,
@@ -29,19 +30,22 @@ CREATE TABLE world_happiness (
 
 CREATE TABLE world_covid_data (
 	covid_cases_id INT PRIMARY KEY,
-    country_id INT NOT NULL,
-	FOREIGN KEY (country_id)REFERENCES systems_govt (id),
+    country VARCHAR,
+	FOREIGN KEY (country)REFERENCES un_govt(id),
     date DATE   NOT NULL,
-    new_cases INT   NOT NULL,
-    new_deaths INT   NOT NULL
+    new_cases INT,
+    new_deaths INT,
+	icu_patients INT 
 );
 
 CREATE TABLE gov_response (
 	response_id INT PRIMARY KEY,
-    country_id INT NOT NULL,
-	FOREIGN KEY (country_id)REFERENCES systems_govt (id),
+	country VARCHAR NOT NULL,
+--     country_id INT NOT NULL,
+	FOREIGN KEY (country)REFERENCES un_govt(country),
     gov_resp_date DATE   NOT NULL,
     gov_resp_desc VARCHAR   NOT NULL,
+	enforcer VARCHAR NOT NULL,
     gov_resp_type VARCHAR   NOT NULL,
     gov_resp_link_src VARCHAR   NOT NULL
 	
